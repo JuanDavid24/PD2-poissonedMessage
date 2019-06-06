f53638df3b677961b6ca9ced9c6d23bd9e2a2116
JuanDavid24 <david.juan90.jd@gmail.com>
2019-06-06 20:04:58 -0300

    Modificado receiver: republica mensajes incrementando attempts y los descarta tras 3 intentos.

--- problem/receiver.py
+++ problem/receiver.py
@@ -3,15 +3,36 @@ import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-sv'))
 channel = connection.channel()
+#args = ({'x-dead-letter-exchange': ''})
 channel.queue_declare(queue='cola')
 
 def callback(ch, method, properties, body):
     recibed = json.loads(body)
-    print("Recibido: " + str(recibed))
-    h = properties.headers
+    print("\nRecibido: " + str(recibed))
+    print("delivery tag: " + str(method.delivery_tag))
 
-    print(type(h)) #output -> <class 'dict'>
-    #print(h['x-delivery-attempts'])    #-> cuando descomento esto la linea de arriba devuelve Nonetype en vez de dict. No puedo acceder a la key
+    try:
+        result = recibed[0] / recibed[1]
+        print ("resultado: " + str(result))
+
+    except:
+        #print(properties.headers)
+        attempts = properties.headers['x-delivery-attempts']
+        if attempts < 3:
+            attempts += 1
+            print("attempts: {at} ".format(at=attempts))
+
+            channel.basic_publish(exchange='',
+                                  routing_key='cola',
+                                  body=json.dumps(recibed),
+                                  properties=pika.BasicProperties(headers={"x-delivery-attempts": attempts})
+                                  )
+            print("republished with x-header modified")
+            channel.basic_ack(method.delivery_tag)
+
+        else:
+            channel.basic_nack(method.delivery_tag, requeue=False)
+            print("message {tag} dead-lettered after {at} attempts".format(at= attempts+1, tag=method.delivery_tag))
 
 
 channel.basic_consume(queue='cola',
--- problem/sender.py
+++ problem/sender.py
@@ -3,13 +3,12 @@ import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-sv'))
 channel = connection.channel()
-
+#args = ({'x-dead-letter-exchange': ''})
 channel.queue_declare(queue='cola')
-props = pika.BasicProperties(headers= {'x-delivery-attempts': 0})
 channel.basic_publish(exchange = '',
                       routing_key = 'cola',
-                      body = json.dumps((6,0)),
-                      properties = props
+                      body = json.dumps((7,0)),
+                      properties = pika.BasicProperties(headers= {"x-delivery-attempts": 0})
                       )
 
 connection.close()
